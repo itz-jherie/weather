9fd5c5ae7dec916e8333ab4786843e84cb3b5341
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-01-14 01:59:32 +0100

    added the more panel

--- src/App.js
+++ src/App.js
@@ -12,29 +12,28 @@ const api = {
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
+  const [error, setError] = useState(null);
 
   const search = e => {
     if (e.key === "Enter") {
-      fetch(`${api.base}/weather?q=${query}&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result =>  {
-          setWeather(result);
-          setQuery('');
-        });
+        fetch(`${api.base}/weather?q=${query}&APPID=${api.key}`)
+          .then(res =>  res.json())
+          .then(result =>  {
+            setWeather(result);
+            setQuery('');
+          if(result.cod == 404 || 400) {
+            setError(result.message)
+          }
+          })
+          .catch(err => {setError(err.message)})
+      
     }
  }
-  return (
+  
+    return (
     <div className={(typeof weather.main != "undefined") ? (((weather.main.temp)-273.15 > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className='country'>{weather.name}, <span>{weather.sys.country}</span></div>
-          <div className='date'>{new Date().toDateString()}</div>
-          <div className= 'temp'>{Math.round((weather.main.temp)-273.15)}°c</div>
-          <div className='desc'>{weather.weather[0].main}</div>
-        </div>
-        ) : ('')}
-        <div className='search-box'>
+      <div className='search-box'>
           <input
           type='text' 
           className='search-bar' 
@@ -45,6 +44,28 @@ function App() {
 
           </input>
         </div>
+        {error && <div className='error'>{ error }</div>}
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className='country'>{weather.name}, <span>{weather.sys.country}</span></div>
+          <div className='date'>{new Date().toDateString()}</div>
+          <div className= 'temp'>{Math.round((weather.main.temp)-273.15)}°c</div>
+          <div className='desc'>{weather.weather[0].main}</div>
+          <div className='container'>
+              <div>
+                <button>More</button>
+                <div className="list-container">
+                  <div className="maxtemp">Max-temp: {(Math.round((weather.main.temp_max)-273.15))}°c</div>
+                  <div className="mintemp">Min-temp: {(Math.round((weather.main.temp_min)-273.15))}°c</div>
+                  <div className="windspeed">Windspeed: {weather.wind.speed}m/s</div>
+                  <div className="humidity">Humidity: {weather.main.humidity}%</div>
+                  <div className="visibility">Pressure: {weather.main.pressure}hPa</div>
+                  <div className="timezone">Visibility: {Math.round(weather.visibility)/ 1000}km</div>
+                </div>
+              </div>
+          </div>
+        </div>
+        ) : ('')}
       </main>
       
  
